6f6a5936fa7da5b4d8a16b120808a3b8778006fe
Raycho Mukelov <raicho.mukelov@gmail.com>
2015-05-20 22:38:16 +0300

    pep8

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include_msvcr": True}
+build_exe_options = {
+    "packages": ["os"],
+    "excludes": ["tkinter"],
+    "include_msvcr": True
+}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -10,8 +14,9 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "IntielIPMultithermostat",
-        version = "1.0",
-        description = "Intiel IP Multithermostat Control Panel",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("main.py", base=base)])
+setup(name="IntielIPMultithermostat",
+      version="1.0",
+      description="Intiel IP Multithermostat Control Panel",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("main.py", base=base)]
+)
